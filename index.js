64fc1da06f6009b6aff9027e4a898a265a3f2f4a
Prabhjot Arora <83449967+prabhjot-arora31@users.noreply.github.com>
2024-08-06 17:20:52 +0530

    Update index.js

--- index.js
+++ index.js
@@ -25,7 +25,7 @@ app.use(cookieParser()); // Uncomment if using cookies
 //  }));
 app.use(
   cors({
-   origin: [ "https://notes-app-3112.vercel.app"],
+   origin: [ 'https://notes-app-3112.netlify.app', "https://notes-app-3112.vercel.app"],
    credentials: true,
   })
 );
